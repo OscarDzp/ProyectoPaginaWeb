7091f404aa8b11a86aac0ed7e6486eb929d0696c
OscarDzp <107152960+OscarDzp@users.noreply.github.com>
2022-10-31 17:24:34 -0600

    update
    limpio de la pagina react

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button> hola </Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         
-        </a>
+      <header>
+        
       </header>
     </div>
   );
