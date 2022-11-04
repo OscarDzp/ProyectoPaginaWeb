7aa363a22d613f708894f34ca41789e8f935aec0
OscarDzp <107152960+OscarDzp@users.noreply.github.com>
2022-11-04 09:09:44 -0600

    javascript y  nav bar
    se realizo la nav bar pero  daunos errores, y falta incorporar iconos

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('#search, .fa-search').mouseenter(function(){
+        $('.logo').hide();
+    });
+
+    $('#search, .fa-search').mouseleave(function(){
+        $('.logo').show();
+    });
+
+    $('.fa-bars').click(function(){
+        $('.navbar').toggle();
+        $(this).toggleClass('fa-times');
+    });
+
+    $('.navbar, .navbar a').click(function(){
+        $('.navbar').hide();
+        $('.fa-bars').removeClass('fa-times');
+    });
+});
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './style.css';
 import Button from 'react-bootstrap/Button';
 function App() {
+
   return (
     <div className="App">
      
