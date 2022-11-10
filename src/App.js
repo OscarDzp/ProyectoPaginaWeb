174ef1f7594a74dc6df96241bd034bee49cc6a63
OscarDzp <107152960+OscarDzp@users.noreply.github.com>
2022-11-10 09:34:40 -0600

    arreglo
    se cambio la pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './style.css';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
     <div className="App">
-     
     </div>
   );
 }
